0790970bc2231af511d9588fdc3e2f7706fd2318
Victor1604 <anonimoestelar1604@gmail.com>
2025-10-30 11:34:24 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,26 @@ function App() {
 
   return (
     <>
-      <h1 className={styles.titulo}>dasdas</h1>
-
+      <nav>
+        <a href="#s1">Home</a>
+        <a href="#s2">Cards</a>
+        <a href="#s3">Video</a>
+        <a href="#s4">Contato</a>
+      </nav>
+      <main>
+        <section>
+          <h2>Sessão 1</h2>
+        </section>
+        <section>
+          <h2>Sessão 2</h2>
+        </section>
+        <section>
+          <h2>Sessão 3</h2>
+        </section>
+        <section>
+          <h2>Sessão 4</h2>
+        </section>
+      </main>
     </>
   )
 }
